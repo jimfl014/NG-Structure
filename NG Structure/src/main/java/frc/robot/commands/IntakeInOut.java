5b1e3317789196298b42f32b99a5451fda4c4cbd
jimfl014 <34219043+jimfl014@users.noreply.github.com>
2019-03-28 10:49:12 -0400

    Add files via upload

---
+++ NG Structure/src/main/java/frc/robot/commands/IntakeInOut.java
@@ -0,0 +1,58 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
+
+public class IntakeInOut extends Command {
+  
+  public IntakeInOut() {
+    // Use requires() here to declare subsystem dependencies
+    // eg. requires(chassis);
+    requires (Robot.m_IntakeInOut);
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+    
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+    if (Robot.intakeOut){ //boolean Robot.intakeOut to track state true = extented out
+
+      Robot.m_IntakeInOut.IntakeDown();
+
+    }
+    else {
+
+      Robot.m_IntakeInOut.IntakeUp();
+
+    }
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return false;
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+  }
+}
---
+++ NG Structure/src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,70 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.Timer;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+
+import frc.robot.Robot;
+import frc.robot.RobotMap;
+/**
+ * Add your docs here.
+ */
+public class Intake extends Subsystem {
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+
+  Timer elapsedTime;
+  VictorSPX intakeMotor;
+  DoubleSolenoid intakeOut;
+  static final DoubleSolenoid.Value RETRACT = DoubleSolenoid.Value.kForward;
+  static final DoubleSolenoid.Value EXTEND = DoubleSolenoid.Value.kReverse;
+  static double motorPower = RobotMap.intakeMotorPower;
+  double startTime;
+  public Intake() {
+
+    intakeMotor = new VictorSPX(RobotMap.intakeMotor);
+    intakeOut = new DoubleSolenoid(RobotMap.intakeOut, RobotMap.intakeIn);
+
+  }
+
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+  
+  }
+public void IntakeUp() {
+
+intakeOut.set(EXTEND);
+startTime = elapsedTime.get();
+  if (startTime + RobotMap.intakeMotorStart <= startTime){
+
+    intakeMotor.set(ControlMode.PercentOutput, motorPower);
+
+  }
+Robot.intakeOut = true; //Used to track the state of the intake arm
+}
+
+public void IntakeDown() {
+
+intakeMotor.set(ControlMode.PercentOutput, 0);
+startTime = elapsedTime.get();
+  if (startTime + RobotMap.intakeMotorStart <= startTime){
+
+    intakeOut.set(RETRACT);
+
+  }
+Robot.intakeOut = false;  //Used to track the state of the intake arm
+}
+
+}
